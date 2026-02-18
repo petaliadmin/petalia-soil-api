857913a06eba446f6b36879776d1a16905394997
Papa Babacar Ngor Segnhor <petaliafarmos@gmail.com>
2026-02-18 13:56:31 +0000

    marketplace

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { UserRole } from '../../common/enums';
+import { ProviderSchema } from '../../market-place/schema/provider.schema';
 
 export type UserDocument = User & Document;
 
@@ -12,7 +13,7 @@ export class User {
   @Prop({ required: true })
   fullName: string;
 
-  @Prop({ required: true, unique: true, lowercase: true })
+  @Prop({ required: true, lowercase: true })
   email: string;
 
   @Prop({ required: true })
@@ -48,3 +49,6 @@ export const UserSchema = SchemaFactory.createForClass(User);
 
 // Index pour améliorer les performances de recherche
 UserSchema.index({ email: 1 });
+
+ProviderSchema.index({ email: 1 }, { unique: true });
+ProviderSchema.index({ accessCode: 1 }, { unique: true, sparse: true });
